ac102d6969887790b3f7f8fbee57a1d124186db1
JonZavialov <74842207+JonZavialov@users.noreply.github.com>
2022-01-15 09:49:48 -0500

    created layout

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# portfolio4
+<h1 align="center">Jonathan Zavialov</h1>
+
+<p align="center">
+<img src="https://img.shields.io/tokei/lines/github/JonZavialov/portfolio4?color=lightgray" alt="Total Lines" />
+<img src="https://img.shields.io/github/repo-size/JonZavialov/portfolio4?color=lightgray&logo=GitHub" alt="GitHub Repo Size" />
+<img src="https://img.shields.io/github/commit-activity/m/JonZavialov/portfolio4?color=lightgray&logo=GitHub" alt="Commit Activity" />
+</p>
+
+<p align="center">My Personal Portfolio<br>Style inspired by Microsoft Windows 98<br>Version 4</p>
+<a href="http://jonzav.me/">View Website</a>
+
+## 👨‍💻 Developlment
+
+This project is a currently in progress by Jonathan.
---
+++ src/js/root/loadNav.js
@@ -0,0 +1,28 @@
+function loadNav(){
+    let navBar = `
+        <li><a href=\"/home\">Home</a></li>
+        <ul>
+            <li style="cursor: pointer;" onmouseup="recycleBin()">Recycle Bin</li>
+            <li style="cursor: pointer;" onmouseup="myComputer()">My Computer</li>
+            <li style="cursor: pointer;" onmouseup="myDocuments()">My Documents</li>
+            <ul>
+                <li style="cursor: pointer;" onmouseup="jonpng()">jon.png</li>
+                <li style="cursor: pointer;" onmouseup="credits()">credits.txt</li>
+                <li style="cursor: pointer;" onmouseup="resume()">resume.pdf</li>
+            </ul>
+            <li style="cursor: pointer;" onmouseup="apps()">My Apps</li>
+            <ul>
+                <li style="cursor: pointer;" onmouseup="openCalculator()">Calculator</li>
+                <li style="cursor: pointer;" onmouseup="txtEditor()">Text Editor</li>
+                <li style="cursor: pointer;" onmouseup="initCalendar()">Calendar</li>
+                <li style="cursor: pointer;" onmouseup="initEmail()">Outlook Express</li>
+                <li style="cursor: pointer;" onmouseup="initMsdos()">MS-DOS Prompt</li>
+                <li style="cursor: pointer;" onmouseup="initProjects()">My Projects</li>
+            </ul>
+            <li style="cursor: pointer;" onmouseup="nft()">My NFTs</li>
+        </ul>
+        <li><a href=\"https://github.com/JonZavialov/portfolio2\" target=\"_blank\">Repository</a></li>
+    `
+
+    $("#treeDisplay").html(navBar)
+}
