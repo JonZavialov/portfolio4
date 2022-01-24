ed586c9dc01cbf9d777bb977cac263aaeefdecd8
JonZavialov <74842207+JonZavialov@users.noreply.github.com>
2022-01-23 21:55:42 -0500

    fixed taskbar styling

--- src/js/icons/clickFunctions/MyComputer.js
+++ src/js/icons/clickFunctions/MyComputer.js
@@ -1,3 +1,22 @@
+class MyComputer extends Window {
+  constructor() {
+    super(
+      "My Computer",
+      "myComputer",
+      true,
+      "assets/images/icons/computer.png"
+    );
+    this.#generate();
+  }
+
+  #generate() {
+    let contents = document.createElement("div");
+    contents.innerHTML = "test";
+    this.generateElement(contents);
+  }
+}
+
 function openMyComputer() {
-  console.log("opened my computer");
+  let myComputer = new MyComputer();
+  myComputer.render();
 }
