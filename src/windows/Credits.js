791d44f886d61a5071afbf23a015d1103a0a3827
JonZavialov <74842207+JonZavialov@users.noreply.github.com>
2022-02-01 21:46:11 -0500

    removed some comments'

--- src/windows/Credits.js
+++ src/windows/Credits.js
@@ -5,9 +5,6 @@ function openCredits() {
   $.ajax({
     url: '/assets/text/credits.txt',
     success(data) {
-      // parse your data here
-      // you can split into lines using data.split('\n')
-      // an use regex functions to effectively parse it\
       const credits = new TextEditor(data, 'credits.txt');
       credits.render();
     },
