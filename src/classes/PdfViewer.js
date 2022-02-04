1194d4844553b08b5c8092dcb2d9cc4384ef1e63
JonZavialov <74842207+JonZavialov@users.noreply.github.com>
2022-02-04 11:33:27 -0500

    added control buttons and zoom in/out functions

--- src/classes/PdfViewer.js
+++ src/classes/PdfViewer.js
@@ -24,6 +24,27 @@ class PdfViewer extends Window {
    * @returns {HTMLElement} - The DOM element of the window.
    */
   getInnerHTML() {
+    const controlButtons = [{
+      image: '/assets/images/icons/backArrow.png',
+      alt: 'Previous Page',
+      margin: "0px",
+      action: () => this.previousPage()
+    }, {
+      image: '/assets/images/icons/forwardArrow.png',
+      alt: 'Next Page',
+      margin: "0px",
+      action: () => this.nextPage()
+    }, {
+      image: '/assets/images/icons/zoomIn.png',
+      alt: 'Zoom In',
+      margin: '10px',
+      action: () => this.zoomIn()
+    }, {
+      image: '/assets/images/icons/zoomOut.png',
+      alt: 'Zoom Out',
+      margin: "10px",
+      action: () => this.zoomOut(),
+    }]
     const container = document.createElement('div');
     container.className = 'pdfContainer';
 
@@ -43,6 +64,20 @@ class PdfViewer extends Window {
 
     const buttonControls = document.createElement('div');
     buttonControls.className = 'buttonControls';
+    controlButtons.forEach((button) => {
+      const buttonDOM = document.createElement('button');
+      buttonDOM.className = "PDFButton";
+      buttonDOM.onclick = button.action;
+      buttonDOM.style.width = `${this.viewerWidth / 8}px`;
+      buttonDOM.style.marginTop = button.margin
+
+      const image = document.createElement('img');
+      image.src = button.image;
+      image.alt = button.alt;
+
+      buttonDOM.append(image)
+      buttonControls.append(buttonDOM)
+    })
 
     const pageControls = document.createElement('div');
     pageControls.className = 'pageControls';
@@ -57,36 +92,63 @@ class PdfViewer extends Window {
    * Generates the PDF.
    */
   generatePDF() {
-    const defaultState = {
+    this.defaultState = {
       pdf: null,
       currentPage: 1,
       zoom: 1,
     };
 
-    // get the pdf
     pdfjsLib.getDocument(this.path).then((pdf) => {
-      defaultState.pdf = pdf;
+      this.defaultState.pdf = pdf;
+      this.renderPDF()
+    });
+  }
 
-      // render the pdf into to the document
-      defaultState.pdf.getPage(defaultState.currentPage).then((page) => {
-        const canvas = $(this.elem).find('#pdf_renderer')[0];
-        const ctx = canvas.getContext('2d');
+  /**
+   * Renders the PDF into the viewer.
+   */
+  renderPDF() {
+    this.defaultState.pdf.getPage(this.defaultState.currentPage).then((page) => {
+      const canvas = $(this.elem).find('#pdf_renderer')[0];
+      const ctx = canvas.getContext('2d');
 
-        const viewport = page.getViewport(defaultState.zoom);
+      const viewport = page.getViewport(this.defaultState.zoom);
 
-        canvas.width = viewport.width;
-        canvas.height = viewport.height;
+      canvas.width = viewport.width;
+      canvas.height = viewport.height;
 
-        $(this.controls).css(
-          'height',
-          `${(this.viewerWidth / viewport.width) * viewport.height}px`
-        );
+      $(this.controls).css(
+        'height',
+        `${(this.viewerWidth / viewport.width) * viewport.height}px`
+      );
 
-        page.render({
-          canvasContext: ctx,
-          viewport,
-        });
+      page.render({
+        canvasContext: ctx,
+        viewport,
       });
     });
+  }
+
+  previousPage() {
+    if (this.defaultState.pdf == null || this.defaultState.currentPage === 1)
+      return;
+    this.defaultState.currentPage -= 1;
+    this.renderPDF();
+  }
+
+  nextPage() {
+    // eslint-disable-next-line no-underscore-dangle
+    if (this.defaultState.pdf == null || this.defaultState.currentPage === this.defaultState.pdf._pdfInfo.numPages)
+      return;
+    this.defaultState.currentPage += 1;
+    this.renderPDF();
+  }
+
+  zoomIn() {
+    console.log('zoom in');
+  }
+
+  zoomOut() {
+    console.log('zoom out');
   }
 }
