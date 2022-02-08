20239a395387c58562d973243014aef20ef2f8cc
JonZavialov <74842207+JonZavialov@users.noreply.github.com>
2022-02-08 10:49:06 -0500

    redid tab styling for calendar'

--- src/windows/Calendar.js
+++ src/windows/Calendar.js
@@ -1,85 +1,57 @@
 class Calendar extends Window {
-  constructor() {
-    super('Calendar', 'calendar', true, 'assets/images/icons/calendar.png');
+    constructor() {
+        super('Calendar', 'calendar', true, 'assets/images/icons/calendar.png');
 
-    this.generateElement(this.generateHTML());
-  }
-
-  toggleCalendarTab(direction) {
-    // TODO: redo the way the tabs are styled
-    const elem = $(this.elem);
-    const borderCovers = [
-      elem.find('#borderCover')[0],
-      elem.find('#borderCoverVertical')[0],
-      elem.find('#borderCoverVertical2')[0],
-    ];
-    for (let i = 0; i < borderCovers.length; i += 1) {
-      const transformX = parseInt(
-        getComputedStyle(borderCovers[i])
-          .getPropertyValue('transform')
-          .split(' ')[4]
-          .slice(0, -1)
-      );
-      const transformY = parseInt(
-        getComputedStyle(borderCovers[i])
-          .getPropertyValue('transform')
-          .split(' ')[5]
-          .slice(0, -1)
-      );
-      if (direction === -1 && transformX < 10) return;
-      if (direction === 1 && transformX > 50) return;
-      borderCovers[i].style.transform = `translate(${
-        transformX + 77 * direction
-      }px, ${transformY}px)`;
+        this.generateElement(this.generateHTML());
+        this.state = "date"
     }
-    if (direction === -1) this.setCalendarToDate();
-    else this.setCalendarToTime();
-  }
 
-  setCalendarToDate() {
-    console.log('setting to date');
-  }
+    toggleCalendarTab(tab) {
+        if (this.state === "date" && tab === "time") {
+            this.state = "time"
+            this.setCalendarToTime()
+        } else if (this.state === "time" && tab === "date") {
+            this.state = "date"
+            this.setCalendarToDate()
+        }
+    }
 
-  setCalendarToTime() {
-    console.log('setting to time');
-  }
+    setCalendarToDate() {
+        $(this.elem).find("#dateButton")[0].style.boxShadow = "inset -1px -1px #ffffff, inset 1px 1px #0a0a0a, inset -2px -2px #dfdfdf, inset 2px 2px #808080";
+        $(this.elem).find("#timeButton")[0].style.boxShadow = null
+    }
 
-  generateHTML() {
-    const container = document.createElement('div');
+    setCalendarToTime() {
+        $(this.elem).find("#timeButton")[0].style.boxShadow = "inset -1px -1px #ffffff, inset 1px 1px #0a0a0a, inset -2px -2px #dfdfdf, inset 2px 2px #808080";
+        $(this.elem).find("#dateButton")[0].style.boxShadow = null
+    }
 
-    const dateButton = document.createElement('button');
-    dateButton.onclick = () => this.toggleCalendarTab(-1);
-    dateButton.innerHTML = 'Date';
-    dateButton.style.outline = 'none';
-    container.append(dateButton);
+    generateHTML() {
+        const container = document.createElement('div');
 
-    const timeButton = document.createElement('button');
-    timeButton.onclick = () => this.toggleCalendarTab(1);
-    timeButton.innerHTML = 'Time';
-    timeButton.style.outline = 'none';
-    container.append(timeButton);
+        const dateButton = document.createElement('button');
+        dateButton.id = "dateButton";
+        dateButton.onclick = () => this.toggleCalendarTab("date");
+        dateButton.innerHTML = 'Date';
+        dateButton.style.outline = 'none';
+        container.append(dateButton);
 
-    const borderCoverHorizontal = document.createElement('div');
-    borderCoverHorizontal.id = 'borderCover';
-    container.append(borderCoverHorizontal);
+        const timeButton = document.createElement('button');
+        timeButton.id = 'timeButton';
+        timeButton.onclick = () => this.toggleCalendarTab("time");
+        timeButton.innerHTML = 'Time';
+        timeButton.style.outline = 'none';
+        container.append(timeButton);
 
-    const borderCoverVertical1 = document.createElement('div');
-    borderCoverVertical1.id = 'borderCoverVertical';
-    container.append(borderCoverVertical1);
+        const calendarContainer = document.createElement('div');
+        calendarContainer.id = 'calendarBody';
+        container.append(calendarContainer);
 
-    const borderCoverVertical2 = document.createElement('div');
-    borderCoverVertical2.id = 'borderCoverVertical2';
-    container.append(borderCoverVertical2);
-
-    const calendarContainer = document.createElement('div');
-    calendarContainer.id = 'calendarBody';
-    container.append(calendarContainer);
-
-    return container;
-  }
+        return container;
+    }
 }
 
 function openCalendar() {
-  const calendar = new Calendar();
-  calendar.render();
+    const calendar = new Calendar();
+    calendar.render();
 }
