fee0007dcc18491d88764a33df7a96358c87293c
JonZavialov <74842207+JonZavialov@users.noreply.github.com>
2022-02-11 18:23:07 -0500

    clicking on clock icon now opens calendar

--- project/src/root/initClock.js
+++ project/src/root/initClock.js
@@ -2,7 +2,6 @@
  * Initializes the clock in the taskbar.
  */
 function initClock() {
-  // TODO: make clicking on clock icon open calendar app
   setInterval(() => {
     if ($('#desktopClockTimeDisplay').length === 0) return;
     const date = new Date().toLocaleTimeString();
