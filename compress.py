24a746907fa38e26a4aad48818359c1e2af61e1f
JonZavialov <74842207+JonZavialov@users.noreply.github.com>
2022-02-11 16:36:51 -0500

    completed css concatenation

--- compress.py
+++ compress.py
@@ -9,11 +9,10 @@ def getDirsList(path):
     return dirs
         
 def concatFiles(dirsList, path):
-    os.system(f'del .\{path}\styles.min.css')
+    os.system(f'del .\{path[1:]}\styles.min.css')
     for dir in dirsList:
         localPath = dir[1:].replace('/', '\\')
         os.system(f'type {os.getcwd() + localPath}\*.css >> .\{path}\styles.min.css')
-        os.system('cls')
 
 dirsList = getDirsList('./project/styles')
 concatFiles(dirsList, "/project")
@@ -22,5 +21,6 @@ concatFiles(dirsList, "/project")
 # url = 'https://www.toptal.com/developers/cssminifier/raw'
 # data = {'input': open('./styles/boot.css', 'rb').read()}
 # response = requests.post(url, data=data)
+
 
 # print(response.text)
