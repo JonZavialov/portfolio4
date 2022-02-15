871cdbb028facd384befa37102669b69cb3dc9b0
JonZavialov <74842207+JonZavialov@users.noreply.github.com>
2022-02-15 14:23:42 -0500

    add jsdoc comment

--- project/src/root/sleep.js
+++ project/src/root/sleep.js
@@ -1,6 +1,7 @@
 /**
  * Sleeps for a given amount of ms.
  * @param  {number} ms - The amount of ms to sleep.
+ * @returns {Promise} - A promise that resolves after the given amount of ms.
  */
 async function sleep(ms) {
   return new Promise((resolve, reject) => {
