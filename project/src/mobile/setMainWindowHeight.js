e07814935fc7718aef647f3a4b9d701ab3a5ab09
JonZavialov <74842207+JonZavialov@users.noreply.github.com>
2022-03-03 11:26:59 -0500

    added mobile taskbar

--- project/src/mobile/setMainWindowHeight.js
+++
@@ -1,6 +0,0 @@
-function setMainWindowHeight() {
-  const height = window.innerHeight;
-  const windowHeight = `${height - 16}px`; // 2 x body margin + 2 x window padding
-
-  $('#mainWindow').css('height', windowHeight);
-}
