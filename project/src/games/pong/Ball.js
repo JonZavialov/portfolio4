91749b85e968aed130d0588bfed43e403d8eb2b9
JonZavialov <74842207+JonZavialov@users.noreply.github.com>
2022-03-15 11:19:43 -0400

    added pong reset function

--- project/src/games/pong/Ball.js
+++ project/src/games/pong/Ball.js
@@ -1,84 +1,80 @@
 class Ball {
-  /**
-   * The Ball.
-   * @param  {Board} board - The board object.
-   * @constructor
-   */
-  constructor(board) {
-    this.radius = 7;
-    this.x = board.width / 2;
-    this.y = board.height / 2;
+    /**
+     * The Ball.
+     * @param  {Board} board - The board object.
+     * @constructor
+     */
+    constructor(board) {
+        this.radius = 7;
+        this.height = board.height;
+        this.width = board.width;
 
-    this.height = board.height;
-    this.width = board.width;
-
-    this.generateInitialVeloctiy();
-    this.boardLoop = board.loop;
-    this.boardScore = board.score;
-  }
-
-  /**
-   * Draws the ball on the canvas.
-   * @param  {Board} board - The board object.
-   */
-  renderIntoBoard(board) {
-    board.beginPath();
-    board.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
-    board.fill();
-  }
-
-  /**
-   * Randomly generates the initial velocity and direction of the ball.
-   */
-  generateInitialVeloctiy() {
-    this.xVelocity = 1.4;
-    this.yVelocity = 1.4;
-
-    if (Math.random() < 0.5) this.xVelocity *= -1;
-    if (Math.random() < 0.5) this.yVelocity *= -1;
-  }
-
-  /**
-   * Moves the ball on the canvas.
-   * @param  {Paddle[]} paddles - The array of paddles on the board.
-   */
-  move(paddles) {
-    // if it hits the top or bottom change direction
-    if (this.y < this.radius || this.y > this.height - this.radius)
-      this.yVelocity = -this.yVelocity;
-
-    // if it goes to the end of the screen restart the game
-    if (this.x < this.radius || this.x > this.width + this.radius) {
-      clearInterval(this.boardLoop);
-
-      const reloadURL = new URL(window.location.href);
-      if (this.x < this.radius) {
-        reloadURL.searchParams.set('aiScore', parseInt(this.boardScore[1]) + 1);
-        reloadURL.searchParams.set('playerScore', this.boardScore[0]);
-      }
-      if (this.x > this.width + this.radius) {
-        reloadURL.searchParams.set(
-          'playerScore',
-          parseInt(this.boardScore[0]) + 1
-        );
-        reloadURL.searchParams.set('aiScore', this.boardScore[1]);
-      }
-      window.location.href = reloadURL.href;
-      // TODO: Make a reset function instead of reloading the page to increase speed
+        this.reset()
+        this.boardScore = board.score;
+        this.board = board;
     }
 
-    this.x += this.xVelocity;
-    this.y += this.yVelocity;
+    /**
+     * Resets the game by centering the ball and recalculating the initial velocity.
+     */
+    reset() {
+        this.x = this.width / 2;
+        this.y = this.height / 2;
+        this.generateInitialVeloctiy()
+    }
 
-    paddles.forEach((paddle) => paddle.checkForHit(this));
-  }
+    /**
+     * Draws the ball on the canvas.
+     * @param  {Board} board - The board object.
+     */
+    renderIntoBoard(board) {
+        board.beginPath();
+        board.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+        board.fill();
+    }
 
-  /**
-   * Checks if the ball is the same height as the paddle.
-   * @param  {Paddle} paddle - The paddle object.
-   * @returns {boolean} - True if the ball is the same height as the paddle.
-   */
-  isSameHeight(paddle) {
-    return this.y >= paddle.y && this.y <= paddle.y + paddle.height;
-  }
+    /**
+     * Randomly generates the initial velocity and direction of the ball.
+     */
+    generateInitialVeloctiy() {
+        this.xVelocity = 1.4;
+        this.yVelocity = 1.4;
+
+        if (Math.random() < 0.5) this.xVelocity *= -1;
+        if (Math.random() < 0.5) this.yVelocity *= -1;
+    }
+
+    /**
+     * Moves the ball on the canvas.
+     * @param  {Paddle[]} paddles - The array of paddles on the board.
+     */
+    move(paddles) {
+        // if it hits the top or bottom change direction
+        if (this.y < this.radius || this.y > this.height - this.radius)
+            this.yVelocity = -this.yVelocity;
+
+
+        // if it goes to the end of the sreen restart the game
+        if (this.x < this.radius || this.x > this.width + this.radius) {
+            if (this.x < this.radius) this.boardScore.ai += 1
+            if (this.x > this.width + this.radius) this.boardScore.player += 1
+
+            this.board.renderScore()
+            this.reset()
+        }
+
+        this.x += this.xVelocity;
+        this.y += this.yVelocity;
+
+        paddles.forEach((paddle) => paddle.checkForHit(this));
+    }
+
+    /**
+     * Checks if the ball is the same height as the paddle.
+     * @param  {Paddle} paddle - The paddle object.
+     * @returns {boolean} - True if the ball is the same height as the paddle.
+     */
+    isSameHeight(paddle) {
+        return this.y >= paddle.y && this.y <= paddle.y + paddle.height;
+    }
 }
--- project/src/games/pong/Board.js
+++ project/src/games/pong/Board.js
@@ -4,18 +4,17 @@ class Board {
    * @constructor
    */
   constructor() {
-    this.canvas = $('#gameBoard')[0].getContext('2d');
-    this.canvas.fillStyle = 'white';
+    this.canvas = $("#gameBoard")[0].getContext("2d");
+    this.canvas.fillStyle = "white";
     this.canvas.lineWidth = 15;
-    this.width = $('#gameBoard').width();
-    this.height = $('#gameBoard').height();
+    this.width = $("#gameBoard").width();
+    this.height = $("#gameBoard").height();
 
     this.paddles = [];
-    const params = new URLSearchParams(window.location.search);
-    this.score = [
-      params.get('playerScore') || '0',
-      params.get('aiScore') || '0',
-    ];
+    this.score = {
+      player: 0,
+      ai: 0,
+    }
     this.renderScore();
   }
 
@@ -23,12 +22,12 @@ class Board {
    * Renders the score counter on the canvas.
    */
   renderScore() {
-    $('#playerScore').text(this.score[0]);
-    $('#playerScore').css(
-      'transform',
-      `translate(${174 - (this.score[0].length - 1) * 18}px, -284px)`
+    $("#playerScore").text(this.score.player);
+    $("#playerScore").css(
+      "transform",
+      `translate(${174 - (this.score.player.toString().length - 1) * 18}px, -284px)`
     );
-    $('#aiScore').text(this.score[1]);
+    $("#aiScore").text(this.score.ai);
   }
 
   /**
@@ -53,9 +52,9 @@ class Board {
    * Clears the canvas.
    */
   clear() {
-    this.canvas.fillStyle = 'black';
+    this.canvas.fillStyle = "black";
     this.canvas.fillRect(0, 0, this.width, this.height);
-    this.canvas.fillStyle = 'white';
+    this.canvas.fillStyle = "white";
 
     this.canvas.fillRect(this.width / 2, 0, 1, this.height);
   }
@@ -73,10 +72,10 @@ class Board {
    * Starts the game loop.
    */
   init() {
-    this.loop = setInterval(() => {
+    setInterval(() => {
       this.ball.move(this.paddles);
       this.paddles.forEach((paddle) => {
-        if (paddle.constructor.name === 'AiPaddle')
+        if (paddle.constructor.name === "AiPaddle")
           paddle.moveToBall(this.ball, this);
       });
       this.update();
