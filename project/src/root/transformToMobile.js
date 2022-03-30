f824c968cc2f0306ce71ddf37223e15ab338fef8
JonZavialov <74842207+JonZavialov@users.noreply.github.com>
2022-03-29 20:27:44 -0400

    added redirect

--- project/src/root/transformToMobile.js
+++ project/src/root/transformToMobile.js
@@ -2,37 +2,5 @@
  * Sets the HTML of the document to an error message.
  */
 function transformToMobile() {
-  const doc = $('body')[0];
-  doc.id = 'bsodBody';
-  doc.innerHTML = `
-    <p id="bsodHeader">
-    System Error
-    </p>
-    <p id="bsodCentered">
-    A fatal exception 0E76534801 has occured at 0027:C87123 
-    in VXD VMM. Operating system has been stopped to prevent
-    damage to your computer.
-    </p>
-    <p id="bsodBottom">
-    * This website is only available on desktop computers.<br><br>
-    * Sorry!
-    </p>
-    <p id="bsodFooter">
-    Press any key to continue
-    </p>
-    `;
-  const footer = $('#bsodFooter')[0];
-  let i = 0;
-  function myLoop() {
-    setTimeout(() => {
-      i += 1;
-      footer.innerHTML =
-        i % 2 === 0
-          ? `${footer.innerHTML.slice(0, -6)}_`
-          : `${footer.innerHTML.slice(0, -1)}&nbsp`;
-      myLoop();
-    }, 300);
-  }
-
-  myLoop();
+  window.location.replace('/social');
 }
