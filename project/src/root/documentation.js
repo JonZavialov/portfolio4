5ef106149de000af97a08936c28afc47b6dc4caf
JonZavialov <74842207+JonZavialov@users.noreply.github.com>
2022-12-01 23:19:24 -0500

    added jsdoc

--- project/src/root/documentation.js
+++ project/src/root/documentation.js
@@ -1,3 +1,6 @@
+/**
+ * Opens the my API documentation in a new window
+ */
 function openDocumentation() {
     window.open('https://api.jonzav.me', '_blank');
     // TODO: make a browser app to view website???
