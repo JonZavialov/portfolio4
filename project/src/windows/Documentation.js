d7106c83ed57a76b2383af9e3c4f80a8644a6466
JonZavialov <74842207+JonZavialov@users.noreply.github.com>
2022-12-11 03:39:18 -0500

    better fix for mouseup event bug

--- project/src/windows/Documentation.js
+++ project/src/windows/Documentation.js
@@ -1,7 +1,11 @@
 /**
  * Opens the documentation app.
  */
-function openDocumentation() {
+async function openDocumentation() {
+    $('.docsIcon').draggable('disable');
+    await sleep(10);
+    $('.docsIcon').draggable('enable');
+    
     const documentation = new InternetExplorer('JonZav API', 'documentation', 'https://api.jonzav.me');
     documentation.render();
 }
