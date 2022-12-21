e3ddb277dbf52a7f0dda9daa8da6b88416e31deb
JonZavialov <74842207+JonZavialov@users.noreply.github.com>
2022-12-20 21:13:28 -0500

    added message to console

---
+++ project/src/root/consoleLog.js
@@ -0,0 +1,10 @@
+/**
+ * Logs a message to the console.
+ */
+function consoleLog() {
+  console.log(
+    '%cHello! \n%cThis project is open source! \nSee the code here: \nhttps://github.com/JonZavialov/portfolio4',
+    'color: #ff0000; font-size: 30px;',
+    'color: #00ff00; font-size: 20px;'
+  );
+}
