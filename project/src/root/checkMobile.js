4281bb6b966813391ae2541dd3fbc50ac1977aed
JonZavialov <74842207+JonZavialov@users.noreply.github.com>
2023-01-23 23:38:15 -0500

    fixed mobile booting bug

--- project/src/root/checkMobile.js
+++ project/src/root/checkMobile.js
@@ -1,6 +1,8 @@
 /**
  * Check if the device is mobile.
+ * @param {function} validators - The validators to run if it is not mobile.
  */
-function checkMobile(){
+function checkMobile(validators){
     if (detectMob()) transformToMobile();
+    else validators();
 }
